93d0c54360ead9ab71a6122d1b160e6f541b063d
Melvin Sanchez <melsava1207@gmail.com>
2024-04-10 16:55:58 -0600

    autMiddleware terminado

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/usersModel')
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    try{
+      //Se obtiene el token
+      token  = req.headers.authorization.split('')[1]
+
+      //se verifica el token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+      //se obtiene los datos del usuario del token
+      req.user = await User.findById(decoded.id_user).select('-password')
+      
+      next()
+    } catch(error){
+      console.log(error)
+      res.status(401)
+      throw new Error('Acceso no autorizado')
+    }
+  }
+
+  if(!token){
+    res.status(401)
+    throw new Error('Acceso no autorizado, no se ha proporcionado un token')
+  }
+})
+
+module.exports = { protect }
